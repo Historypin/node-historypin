2d0bd4ed9bb81ff3e4f1233b8283bcd24abd6a17
dan-nl <d_entous@yahoo.com>
2016-08-31 02:07:12 +0200

    re-factored
    * used an object for the endpoints instead of a switch case
    * now throws errors instead of console.warn

--- src/get-api-endpoint.js
+++ src/get-api-endpoint.js
@@ -1,50 +1,44 @@
 'use strict';
 
 /**
+ * module variables
+ */
+var endpoints = {
+  'get-gallery': 'https://www.historypin.org/en/api/%s/pin/get_gallery.json',
+  'get-map': 'https://www.historypin.org/en/api/%s/pin/get_map.json',
+  'get-pin': 'https://www.historypin.org/en/api/%s/pin/get.json',
+  'get-project': 'https://www.historypin.org/en/api/%s/projects/get.json',
+  'listing': 'https://www.historypin.org/en/api/projects/listing.json'
+};
+
+/**
  * @param {Object} user_options
  * @param {string} user_options.api_endpoint
  * @param {string} user_options.project
  *
+ * @throws {Error}
+ *
  * @returns {string|undefined}
  */
-module.exports = function getApiEdpoint( user_options ) {
+module.exports = function getApiEndpoint( user_options ) {
   var result;
 
   if ( !( user_options instanceof Object ) ) {
-    console.warn( 'getApiEdpoint(): user_options not provided as an Object' );
-    return;
+    throw new Error( 'getApiEndpoint(): user_options not provided as an Object' );
   }
 
   if ( typeof user_options.api_endpoint !== 'string' ) {
-    console.warn( 'getApiEdpoint(): user_options.api_endpoint not provided as a string' );
-    return;
+    throw new Error( 'getApiEndpoint(): user_options.api_endpoint not provided as a string' );
   }
 
   if ( typeof user_options.project !== 'string' ) {
-    console.warn( 'getApiEdpoint(): user_options.project not provided as a string' );
-    return;
+    throw new Error( 'getApiEndpoint(): user_options.project not provided as a string' );
   }
 
-  switch ( user_options.api_endpoint ) {
-    case 'get-gallery':
-      result = 'http://www.historypin.org/en/api/%s/pin/get_gallery.json';
-      break;
+  result = endpoints[ user_options.api_endpoint ];
 
-    case 'get-map':
-      result = 'http://www.historypin.org/en/api/%s/pin/get_map.json';
-      break;
-
-    case 'get-pin':
-      result = 'http://www.historypin.org/en/api/%s/pin/get.json';
-      break;
-
-    case 'get-project':
-      result = 'http://www.historypin.org/en/api/%s/projects/get.json';
-      break;
-
-    case 'listing':
-      result = 'http://www.historypin.org/en/api/projects/listing.json';
-      break;
+  if ( typeof result !== 'string' ) {
+    throw new Error( 'getApiEndpoint(): api endpoint `' + user_options.api_endpoint + '` is not yet handled' );
   }
 
   return result.replace( '%s', user_options.project );
